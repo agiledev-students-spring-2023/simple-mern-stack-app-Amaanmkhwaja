347631b8e89cd3e813fdcd1120b2be8b32f96748
Amaanmkhwaja <amaanmkhwaja@gmail.com>
2023-02-14 16:03:43 -0500

    added about.js route and js file

---
+++ front-end/src/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { CustomPlaceholder } from 'react-placeholder-image';
+
+function About () {
+  return (
+    <div>
+        <h1>Amaan Khwaja</h1>
+        {/* 👇 show image */}
+        {/* <img src={myImage} alt="Trees" height="200" /> */}
+        <p>hi</p>
+    </div>
+  )
+}
+
+export default About
